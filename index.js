902372d6707fbc7011fdd83b9c36ce82ea383cd2
Vadim <vloginov@users.noreply.github.com>
2016-11-28 09:18:19 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+/*
+  MIT License http://www.opensource.org/licenses/mit-license.php
+  Author Vadim Loginov @vadimatp
+*/
+module.exports = function(source) {
+  var data;
+  var currentConstantName;
+  var currentConstantKey;
+  var constantNames;
+  var currentConstantKeys;
+  var output;
+  var moduleName;
+  try {
+    data = JSON.parse(source);
+  } catch (e) {
+    this.emitError(this.resourcePath + ' must be a valid json object');
+    return '';
+  }
+  console.log('this.query', this.query);
+  moduleName = this.query.moduleName || 'constants';
+  standalone = this.query.standalone || true;
+
+  constantNames = Object.keys(data);
+  output = 'angular.module("' + moduleName + '"' + (standalone ? ', []' : '') + ')\n';
+  for (currentConstantName = 0; currentConstantName < constantNames.length; currentConstantName++) {
+    output += '\n  .constant("' + constantNames[currentConstantName] + '", {\n';
+    currentConstantKeys = Object.keys(data[constantNames[currentConstantName]]);
+    for (currentConstantKey = 0; currentConstantKey < currentConstantKeys.length; currentConstantKey++) {
+      output += '    "' + currentConstantKeys[currentConstantKey] + '": "' +
+        data[constantNames[currentConstantName]][currentConstantKeys[currentConstantKey]];
+      if (currentConstantKey + 1 !== currentConstantKeys.length) {
+        output += '",\n';
+      } else {
+        output += '"\n';
+      }
+    }
+    output += '  })';
+  }
+
+  output += ';';
+
+  console.log(output);
+
+  return output;
+};
